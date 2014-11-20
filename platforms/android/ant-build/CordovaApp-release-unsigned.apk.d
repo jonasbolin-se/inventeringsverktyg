cbcd53d58cb66b4eba47312dd674d660ce6ed37d
Sato Jonas Bolin <bolin.jonas@gmail.com>
2014-11-20 20:10:35 +0100

    2nd commit

---
+++ platforms/android/ant-build/CordovaApp-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/Users/jonasbolin/Development/vbk/platforms/android/ant-build/CordovaApp-release-unsigned.apk : \
+/Users/jonasbolin/Development/vbk/platforms/android/ant-build/CordovaApp.ap_ \
+/Users/jonasbolin/Development/vbk/platforms/android/ant-build/classes.dex \
+/Users/jonasbolin/Development/vbk/platforms/android/CordovaLib/ant-build/classes.jar \
